3a6b8011d4956fc7fe514d6272652c0f0dd6b06a
antony0596 <antony0596@hotmail.com>
2023-04-16 23:28:25 -0600

    Created a GUI and compiled an .exe

---
+++ README.md
@@ -0,0 +1,9 @@
+#YouTube to MP3#
+
+Simple application to download audio tracks from YouTube videos.
+
+Please note that this application is not intended to be used for illegal purposes.
+
+If you are to download a video that is not yours, please make sure you have the rights to do so.
+
+As of right now, I am still learning to program and have not yet figured out how to force royalty-free only downloads.
--- main.py
+++ main.py
@@ -1,70 +1,80 @@
 import os
-from pytube import YouTube
-from pytube import Playlist
 
-def download_song():
-    # download a single video from youtube, audio only
-    video_url = input('Paste your video URL here: ')
-    yt = YouTube(video_url)
-    stream = yt.streams.filter(only_audio=True).first()
-    stream.download()
+from pytube import YouTube, Playlist
 
-    # if music folder doesn't exist, create it
-    if not os.path.exists('music'):
-        os.makedirs('music')
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
-    # change the file name extension to mp3
-    for file in os.listdir():
-        if file.endswith('.mp4'):
-            os.rename(file, f'{file[:-4]}.mp3')
+class YT2MP3(App):
+    def build(self):
+        self.window = GridLayout()
+        self.window.cols = 1
+        self.window.size_hint = (0.6, 0.7)
+        self.window.pos_hint = {'center_x': 0.5, 'center_y': 0.5}
 
-    # move the file to the music folder, if it exists in destination, replace it
-    for file in os.listdir():
-        if file.endswith('.mp3'):
-            os.replace(file, f'music/{file}')
+        # image widget
+        self.window.add_widget(Image(source='icon.png'))
 
-def download_playlist():
-    # download a full playlist from youtube audio only
-    print('The playslist must be public (not private or unlisted)')
-    playlist_url = input('Type your playlist URL here: ')
-    playlist = Playlist(playlist_url)
-    for video in playlist.videos:
-        stream = video.streams.filter(only_audio=True).first()
-        stream.download()
+        # label widget
+        self.app_title = Label(text='YouTube to MP3', font_size=20, color='#14edef')
+        self.window.add_widget(self.app_title)
 
-    # if playlists folder doesn't exist, create it
-    if not os.path.exists('playlists'):
-        os.makedirs('playlists')
+        # text input widget
+        self.url_input = TextInput(multiline=False, hint_text='Enter YouTube URL', font_size=12, padding_y=(20, 20), size_hint=(1, 0.5))
+        self.window.add_widget(self.url_input)
 
-    # change the file name extension to mp3
-    for file in os.listdir():
-        if file.endswith('.mp4'):
-            name = file
-            os.rename(file, f'{file[:-4]}.mp3')
+        # button widget
+        self.download_button = Button(text='Download', size_hint=(0.5, 0.5), bold=True, background_color='#4e26c6')
+        self.download_button.bind(on_press=self.my_callback)
+        self.window.add_widget(self.download_button)
 
-    # get the playlist name
-    playlist_name = playlist.title
+        # label widget
+        self.status_label = Label(text='Status: ')
+        self.window.add_widget(self.status_label)
 
-    # if a folder with the playlist name doesn't exist, create it
-    if not os.path.exists(f'playlists/{playlist_name}'):
-        os.makedirs(f'playlists/{playlist_name}')
+        return self.window
 
-    # move the file to the playlist_name folder, if it exists in destination, replace it
-    for file in os.listdir():
-        if file.endswith('.mp3'):
-            os.replace(file, f'playlists/{playlist_name}/{file}')
+    def my_callback(self, url_input):
+        url = self.url_input.text
 
-def main():
-    print('''
-    1. Download a song
-    2. Download a playlist
-    ''')
-    choice = input('What do you want to do? ')
-    if choice == '1':
-        download_song()
-    elif choice == '2':
-        download_playlist()
-    else:
-        print('Invalid choice.')
+        def is_playlist(url):
+            if "playlist?list=" in url:
+                return "playlist"
+            elif "watch?v=" in url:
+                return "video"
+            else:
+                return False
+            
+        is_playlist = is_playlist(url)
 
-main()
+        if is_playlist == "playlist":
+            self.status_label.text = 'Status: Downloading playlist...'
+            playlist = Playlist(url)
+            playlist_name = playlist.title
+            if not os.path.exists('playlists'):
+                os.makedirs('playlists')
+            if not os.path.exists(f'playlists/{playlist_name}'):
+                os.makedirs(f'playlists/{playlist_name}')
+            for video in playlist.videos:
+                self.status_label.text = f'''Status: Downloading playlist...
+                {video.title}...'''
+                stream = video.streams.filter(only_audio=True).first()
+                stream.download(filename=f"playlists/{playlist_name}/{video.title}.mp3")
+
+        elif is_playlist == "video":
+            self.status_label.text = 'Status: Downloading video...'
+            yt = YouTube(url)
+            if not os.path.exists('music'):
+                os.makedirs('music')
+            stream = yt.streams.filter(only_audio=True).first()
+            stream.download(filename=f"music/{yt.title}.mp3")
+
+        else:
+            self.status_label.text = "Status: Invalid URL"
+
+if __name__ == '__main__':
+    YT2MP3().run()
